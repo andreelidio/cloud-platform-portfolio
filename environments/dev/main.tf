e6b7a4cdb6dc8bd236008053ba364e5e973c16f1
andreelidio <andreelidio@gmail.com>
2026-07-22 16:23:12 -0300

    create public route table and associate public subnet

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,10 +1,9 @@
 module "network" {
-
   source = "../../modules/network"
 
-  name = var.network_name
-
-  cidr = var.network_cidr
+  name               = var.network_name
+  cidr               = var.network_cidr
+  availability_zones = var.availability_zones
 
   tags = local.common_tags
 
