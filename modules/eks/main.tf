4bc7f8db023d17a5a420c1cc8bbeba1cd08797f7
andreelidio <andreelidio@gmail.com>
2026-07-24 14:36:01 -0300

    update files

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -35,11 +35,13 @@ resource "aws_iam_role_policy_attachment" "cluster_policy_attachment" {
 #EKS Cluster
 resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
-  version = var.kubernetes_version
+  version  = var.kubernetes_version
   role_arn = aws_iam_role.cluster.arn
 
   vpc_config {
-    subnet_ids = var.private_subnet_ids
+    subnet_ids              = var.private_subnet_ids
+    endpoint_private_access = var.endpoint_private_access
+    endpoint_public_access  = var.endpoint_public_access
   }
 
   tags = merge(
@@ -53,3 +55,79 @@ resource "aws_eks_cluster" "this" {
     aws_iam_role_policy_attachment.cluster_policy_attachment
   ]
 }
+
+# Node assume role
+data "aws_iam_policy_document" "node_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = [
+      "sts:AssumeRole"
+    ]
+  }
+}
+
+resource "aws_iam_role" "nodes" {
+  name               = "${var.cluster_name}-node-role"
+  assume_role_policy = data.aws_iam_policy_document.node_assume_role.json
+
+  tags = merge(
+    local.common_tags,
+    {
+      "Name" = "${var.cluster_name}-node-role"
+    }
+  )
+}
+
+#Policy attachments for the node role
+resource "aws_iam_role_policy_attachment" "nodes_worker" {
+  role       = aws_iam_role.nodes.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "nodes_ecr" {
+  role       = aws_iam_role.nodes.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "nodes_cni" {
+  role       = aws_iam_role.nodes.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+# Managed Node Group
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "${var.cluster_name}-node-group"
+  node_role_arn   = aws_iam_role.nodes.arn
+
+  subnet_ids = var.private_subnet_ids
+
+  instance_types = var.node_instance_types
+  capacity_type  = var.node_capacity_type
+
+  scaling_config {
+    desired_size = var.node_desired_size
+    max_size     = var.node_max_size
+    min_size     = var.node_min_size
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = var.cluster_name
+    }
+  )
+
+  depends_on = [
+    aws_iam_role_policy_attachment.nodes_worker,
+    aws_iam_role_policy_attachment.nodes_ecr,
+    aws_iam_role_policy_attachment.nodes_cni
+  ]
+}
+
--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -8,24 +8,60 @@ variable "kubernetes_version" {
   type        = string
 }
 
-variable "vpc_id" {
-  description = "The ID of the VPC where the EKS cluster will be deployed"
-  type        = string
-}
-
 variable "private_subnet_ids" {
-  description = "A list of subnet IDs for the EKS cluster"
+  description = "A list of private subnet IDs for the EKS cluster"
   type        = list(string)
-}
-
-variable "public_subnet_ids" {
-  description = "A list of public subnet IDs for the EKS cluster"
-  type        = list(string)
-  default     = []
 }
 
 variable "tags" {
-  description = "Additional tags to assign to network resources"
+  description = "Additional tags to assign to EKS resources"
   type        = map(string)
   default     = {}
 }
+
+variable "node_instance_types" {
+  description = "EC2 instance types used by the EKS managed node group"
+  type        = list(string)
+}
+
+variable "node_capacity_type" {
+  description = "Capacity type used by the EKS managed node group"
+  type        = string
+  default     = "ON_DEMAND"
+
+  validation {
+    condition = contains(
+      ["ON_DEMAND", "SPOT"],
+      var.node_capacity_type
+    )
+    error_message = "node_capacity_type must be ON_DEMAND or SPOT."
+  }
+}
+
+variable "node_desired_size" {
+  description = "Desired number of worker nodes"
+  type        = number
+}
+
+variable "node_min_size" {
+  description = "Minimum number of worker nodes"
+  type        = number
+}
+
+variable "node_max_size" {
+  description = "Maximum number of worker nodes"
+  type        = number
+}
+
+variable "endpoint_private_access" {
+  description = "Enable private access to the EKS Kubernetes API server endpoint"
+  type        = bool
+  default     = true
+}
+
+variable "endpoint_public_access" {
+  description = "Enable public access to the EKS Kubernetes API server endpoint"
+  type        = bool
+  default     = true
+}
+
